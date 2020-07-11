f11eb492dc00cbb3556ae1999ae293e98bd6d8b1
hchung14 <56741854+hchung14@users.noreply.github.com>
2020-07-11 01:42:01 -0700

    display dummy data test

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Milton',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 22, 2020'
+    },
+    {
+        'author': 'Emily',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 23, 2020'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
